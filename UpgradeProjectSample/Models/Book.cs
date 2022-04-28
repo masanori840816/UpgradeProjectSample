e2b5c73b180401c2214f85d070a949a10e22560b
Masui Masanori <masanori840816@gmail.com>
2022-04-29 08:41:12 +0900

    Modelクラスの日付型をver.6.0に合わせて更新

--- UpgradeProjectSample/Models/Book.cs
+++ UpgradeProjectSample/Models/Book.cs
@@ -1,54 +1,67 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace UpgradeProjectSample.Models
+namespace UpgradeProjectSample.Models;
+[Table("book")]
+public class Book
 {
-    [Table("book")]
-    public class Book
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [Column("name")]
+    public string Name { get; set; } = "";
+    [Required]
+    [Column("author_id")]
+    public int AuthorId { get; set; }
+    [Required]
+    [Column("language_id")]
+    public int LanguageId { get; set; }
+    private DateOnly? purchaseDate;
+    [JsonIgnore]
+    [Column("purchase_date", TypeName = "date")]
+    public DateOnly? PurchaseDate
     {
-        [Key]
-        [Column("id")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        [Required]
-        [Column("name")]
-        public string Name { get; set; } = "";
-        [Required]
-        [Column("author_id")]
-        public int AuthorId { get; set; }
-        [Required]
-        [Column("language_id")]
-        public int LanguageId { get; set; }
-        private DateTime? purchaseDate;
-        [JsonIgnore]
-        [Column("purchase_date", TypeName = "date")]
-        public DateTime? PurchaseDate
-        {
-            get { return this.purchaseDate; }
-            set { this.purchaseDate = value; }
-        }
-
-        [Column("price", TypeName = "money")]
-        public decimal? Price { get; set; }
-        [Required]
-        [Column("last_update_date", TypeName = "timestamp with time zone")]
-        public DateTime LastUpdateDate { get; set; }
-        public Author Author { get; set; } = new Author();
-        public Language Language { get; set; } = new Language();
-
-        public static Book Create(Author author, Book value)
-        {
-            return new Book
+        get { return this.purchaseDate; }
+        set { this.purchaseDate = value; }
+    }
+    [NotMapped]
+    public DateTime? PurchaseDateTime
+    {
+        get { return (this.purchaseDate == null)?
+            null: this.purchaseDate.Value.ToDateTime(new TimeOnly(0)); }
+        set {
+            if(value == null)
             {
-                Name = value.Name,
-                AuthorId = author.Id,
-                LanguageId = value.LanguageId,
-                PurchaseDate = value.PurchaseDate,
-                Price = value.Price,
-                LastUpdateDate = DateTime.Now.ToUniversalTime(),
-            };
+                this.purchaseDate = null;
+            }
+            else
+            {
+                this.purchaseDate = DateOnly.FromDateTime(value.Value);
+            }
         }
+    }
+
+    [Column("price", TypeName = "money")]
+    public decimal? Price { get; set; }
+    [Required]
+    [Column("last_update_date", TypeName = "timestamp with time zone")]
+    public DateTime LastUpdateDate { get; set; }
+    public Author Author { get; set; } = new Author();
+    public Language Language { get; set; } = new Language();
+
+    public static Book Create(Author author, Book value)
+    {
+        return new Book
+        {
+            Name = value.Name,
+            AuthorId = author.Id,
+            LanguageId = value.LanguageId,
+            PurchaseDate = value.PurchaseDate,
+            Price = value.Price,
+            LastUpdateDate = DateTime.Now.ToUniversalTime(),
+        };
     }
 }
--- UpgradeProjectSample/Users/Models/ApplicationUser.cs
+++ UpgradeProjectSample/Users/Models/ApplicationUser.cs
@@ -1,119 +1,119 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
-namespace UpgradeProjectSample.Users.Models
+namespace UpgradeProjectSample.Users.Models;
+[Table("application_user")]
+public class ApplicationUser: IdentityUser<int>
 {
-    [Table("application_user")]
-    public class ApplicationUser: IdentityUser<int>
-    {
-        [Key]
-        [Column("id")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public override int Id { get; set; }
-        [Required]
-        [Column("user_name")]
-        public override string UserName { get; set; } = null!;
-        [Column("organization")]
-        public string? Organization { get; set; }
-        [Required]
-        [Column("mail")]
-        public override string Email { get; set; } = null!;
-        [Required]
-        [Column("password")]
-        public override string PasswordHash { get; set; } = null!;
-        [Required]
-        [Column("last_update_date", TypeName = "timestamp with time zone")]
-        public DateTime LastUpdateDate { get; set; }
-        [NotMapped]
-        public override bool EmailConfirmed { get; set; }
-        [NotMapped]
-        public override string NormalizedUserName {
-            get
-            {
-                return UserName.ToUpper();
-            }
-            set { /* DO nothing*/ }
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public override int Id { get; set; }
+    [Required]
+    [Column("user_name")]
+    public override string UserName { get; set; } = null!;
+    [Column("organization")]
+    public string? Organization { get; set; }
+    [Required]
+    [Column("mail")]
+    public override string Email { get; set; } = null!;
+    [Required]
+    [Column("password")]
+    public override string PasswordHash { get; set; } = null!;
+    [Required]
+    [Column("last_update_date", TypeName = "timestamp with time zone")]
+    public DateTime LastUpdateDate { get; set; }
+    [NotMapped]
+    public override bool EmailConfirmed { get; set; }
+    [NotMapped]
+    public override string NormalizedUserName {
+        get
+        {
+            return UserName.ToUpper();
         }
-        [NotMapped]
-        public override string NormalizedEmail {
-            get
-            {
-                return Email.ToUpper();
-            }
-            set { /* DO nothing*/ }
+        set { /* DO nothing*/ }
+    }
+    [NotMapped]
+    public override string NormalizedEmail {
+        get
+        {
+            return Email.ToUpper();
         }
-        [NotMapped]
-        public override bool LockoutEnabled { get; set; }
-        [NotMapped]
-        public override int AccessFailedCount { get; set; }
-        [NotMapped]
-        public override string? PhoneNumber { get; set; }
-        [NotMapped]
-        public override string? ConcurrencyStamp { get; set; }
-        [NotMapped]
-        public override string? SecurityStamp { get; set; }
-        [NotMapped]
-        public override DateTimeOffset? LockoutEnd { get; set; }
-        [NotMapped]
-        public override bool TwoFactorEnabled { get; set; }
-        [NotMapped]
-        public override bool PhoneNumberConfirmed { get; set; }
+        set { /* DO nothing*/ }
+    }
+    [NotMapped]
+    public override bool LockoutEnabled { get; set; }
+    [NotMapped]
+    public override int AccessFailedCount { get; set; }
+    [NotMapped]
+    public override string? PhoneNumber { get; set; }
+    [NotMapped]
+    public override string? ConcurrencyStamp { get; set; }
+    [NotMapped]
+    public override string? SecurityStamp { get; set; }
+    [NotMapped]
+    public override DateTimeOffset? LockoutEnd { get; set; }
+    [NotMapped]
+    public override bool TwoFactorEnabled { get; set; }
+    [NotMapped]
+    public override bool PhoneNumberConfirmed { get; set; }
 
-        public static ApplicationUser CreateSample(string userName, string email, string password)
+    public static ApplicationUser CreateSample(string userName, string email, string password)
+    {
+        var result = new ApplicationUser
         {
-            var result = new ApplicationUser
-            {
-                UserName = userName,
-                Email = email,
-            };
-            result.PasswordHash = new PasswordHasher<ApplicationUser>()
-                .HashPassword(result, password);
-            return result;
+            UserName = userName,
+            Email = email,
+        };
+        result.PasswordHash = new PasswordHasher<ApplicationUser>()
+            .HashPassword(result, password);
+        return result;
+    }
+    public static ApplicationUser Copy(ApplicationUser user)
+    {
+        return new ApplicationUser
+        {
+            Id = user.Id,
+            UserName = user.UserName,
+            Organization = user.Organization,
+            Email = user.Email,
+            LastUpdateDate = DateTime.Now.ToUniversalTime(),
+        };
+    }    
+    public void Update(ApplicationUser user)
+    {
+        this.UserName = user.UserName;
+        this.Organization = user.Organization;
+        this.Email = user.Email;
+        this.PasswordHash = user.PasswordHash;
+        this.LastUpdateDate = DateTime.Now.ToUniversalTime();
+    }
+    public void Update(string userName, string organization,
+        string email, string password)
+    {
+        this.UserName = userName;
+        this.Organization = organization;
+        this.Email = email;
+        // set hashed password text to PasswordHash.
+        this.PasswordHash = new PasswordHasher<ApplicationUser>()
+            .HashPassword(this, password);
+        this.LastUpdateDate = DateTime.Now.ToUniversalTime();
+    }
+    public string? Validate()
+    {
+        if(string.IsNullOrEmpty(UserName))
+        {
+            return "UserName is required";
         }
-        public static ApplicationUser Copy(ApplicationUser user)
+        if(string.IsNullOrEmpty(Email))
         {
-            return new ApplicationUser
-            {
-                Id = user.Id,
-                UserName = user.UserName,
-                Organization = user.Organization,
-                Email = user.Email,
-            };
-        }    
-        public void Update(ApplicationUser user)
-        {
-            UserName = user.UserName;
-            Organization = user.Organization;
-            Email = user.Email;
-            PasswordHash = user.PasswordHash;
+            return "E-Mail address is required";
         }
-        public void Update(string userName, string organization,
-            string email, string password)
+        if(string.IsNullOrEmpty(PasswordHash))
         {
-            UserName = userName;
-            Organization = organization;
-            Email = email;
-            // set hashed password text to PasswordHash.
-            PasswordHash = new PasswordHasher<ApplicationUser>()
-                .HashPassword(this, password);
+            return "Password is required";
         }
-        public string? Validate()
-        {
-            if(string.IsNullOrEmpty(UserName))
-            {
-                return "UserName is required";
-            }
-            if(string.IsNullOrEmpty(Email))
-            {
-                return "E-Mail address is required";
-            }
-            if(string.IsNullOrEmpty(PasswordHash))
-            {
-                return "Password is required";
-            }
-            return null;
-        }
+        return null;
     }
 }
