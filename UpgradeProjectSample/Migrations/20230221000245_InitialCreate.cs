970962815d9ddd2d7d4014fed3399ceda0c24e46
Masui Masanori <Masui.Masanori@ma.mee.co.jp>
2023-02-21 09:04:15 +0900

    add migration files

---
+++ UpgradeProjectSample/Migrations/20230221000245_InitialCreate.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UpgradeProjectSample.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "application_user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_name = table.Column<string>(type: "text", nullable: false),
+                    organization = table.Column<string>(type: "text", nullable: true),
+                    mail = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    last_update_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_application_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "author",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_author", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "language",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_language", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "book",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    author_id = table.Column<int>(type: "integer", nullable: false),
+                    language_id = table.Column<int>(type: "integer", nullable: false),
+                    purchase_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    price = table.Column<decimal>(type: "money", nullable: true),
+                    last_update_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_book_author_author_id",
+                        column: x => x.author_id,
+                        principalTable: "author",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_book_language_language_id",
+                        column: x => x.language_id,
+                        principalTable: "language",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "language",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "English" },
+                    { 2, "Japanese" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_application_user_mail",
+                table: "application_user",
+                column: "mail",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_author_id",
+                table: "book",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_language_id",
+                table: "book",
+                column: "language_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "application_user");
+
+            migrationBuilder.DropTable(
+                name: "book");
+
+            migrationBuilder.DropTable(
+                name: "author");
+
+            migrationBuilder.DropTable(
+                name: "language");
+        }
+    }
+}
