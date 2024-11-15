1fcf21020b2f7a34047c61ee15e0cf8a573484d1
Masui <Masui.Masanori@ma.mee.co.jp>
2024-11-15 13:51:17 +0900

    make simplified

--- UpgradeProjectSample/Books/SearchBooks.cs
+++ UpgradeProjectSample/Books/SearchBooks.cs
@@ -1,35 +1,19 @@
 using UpgradeProjectSample.Books.Dto;
 using UpgradeProjectSample.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace UpgradeProjectSample.Books
 {
-    public class SearchBooks: ISearchBooks
+    public class SearchBooks(SampleContext context): ISearchBooks
     {
-        private readonly ILogger<SearchBooks> logger;
-        private readonly SampleContext context;
-
-        public SearchBooks(ILogger<SearchBooks> logger,
-            SampleContext context)
-        {
-            this.logger = logger;
-            this.context = context;
-        }
-        public async Task<List<Book>> GetAllAsync()
-        {
-            return await this.context.Books
+        public async Task<List<Book>> GetAllAsync() => await context.Books
                 .Include(b => b.Author)
                 .Include(b => b.Language)
                 .ToListAsync();
-        }
         public async Task<List<SearchedBook>> GetAsync(SearchBookCriteria criteria)
         {
             // this.context.SearchedBooks.FromSql から変更.
-            var query = this.context.SearchedBooks
+            var query = context.SearchedBooks
                 .FromSqlRaw("SELECT b.id AS \"BookId\", b.language_id AS \"LanguageId\", b.name AS \"BookName\", a.name AS \"AuthorName\" FROM book b INNER JOIN author AS a ON b.author_id = a.id");
 
             if(string.IsNullOrEmpty(criteria.Name) == false)
@@ -50,14 +34,14 @@ namespace UpgradeProjectSample.Books
         }
         public async Task<List<Book>> GetByLanguageIdsAsync(int[] languageIds)
         {
-            var languages = await this.context.Languages
+            var languages = await context.Languages
                 .Where(L => languageIds.Contains(L.Id))
                 .ToListAsync();
             if(languages.Count <= 0)
             {
-                return new List<Book>();
+                return [];
             }
-            return await this.context.Books
+            return await context.Books
                 .Where(b => languageIds.Contains(b.LanguageId))
                 .ToListAsync();
         }
